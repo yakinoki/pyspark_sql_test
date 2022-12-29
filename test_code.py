d8ee56f6af8ab1a7f09c9155d339d1e67f653efb
unknown <yakinoki>
2022-12-29 15:09:50 +0900

    update_code

--- test_code.py
+++ test_code.py
@@ -1,5 +1,7 @@
-from pyspark.sql.functions import *
+# from pyspark.sql.functions import * と書く事も多い。
+from pyspark.sql import functions as F
 from pyspark.sql import SparkSession
+
 
 filename = 'test.csv'
 spark = SparkSession.builder \
@@ -7,5 +9,17 @@ spark = SparkSession.builder \
 .appName("app") \
 .getOrCreate()
 
-data = spark.read.csv(filename, header=True, inferSchema=True, sep=';')
+# sepを；にすると、全ての列が一つにまとめられてしまう。
+data = spark.read.csv(filename, header=True, inferSchema=True, sep=',')
 data.show()
+
+name = data.select(F.col("name"))
+name.show()
+
+
+data.registerTempTable('users')
+df = spark.sql('SELECT * FROM users').withColumn('spark_user', F.lit(True))
+df.show()
+
+
+
